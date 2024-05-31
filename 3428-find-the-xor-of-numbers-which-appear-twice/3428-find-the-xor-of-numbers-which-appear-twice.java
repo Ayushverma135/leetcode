4c8cc0a2278780a072fd34440ebad5c18c2248ec
Ayush verma <120269805+Ayushverma135@users.noreply.github.com>
2024-05-31 17:19:06 +0530

    Time: 1 ms (99.8%), Space: 43.5 MB (18.48%) - LeetHub

--- 3428-find-the-xor-of-numbers-which-appear-twice/3428-find-the-xor-of-numbers-which-appear-twice.java
+++ 3428-find-the-xor-of-numbers-which-appear-twice/3428-find-the-xor-of-numbers-which-appear-twice.java
@@ -1,4 +1,4 @@
-class Solution {
+class Solution2 {
     public int duplicateNumbersXOR(int[] nums) {
         HashMap<Integer,Integer> map=new HashMap<>();
         for(int i:nums){
@@ -13,6 +13,21 @@ class Solution {
         for(int i:map.keySet()){
             if(map.get(i)>1){
                 res^=i;
+            }
+        }
+        return res;
+    }
+}
+class Solution {
+    public int duplicateNumbersXOR(int[] nums) {
+        int arr[]=new int[51];
+        int res=0;
+        for(int i:nums){
+            if(arr[i]!=0){
+                res^=i;
+            }
+            else{
+                arr[i]=1;
             }
         }
         return res;
