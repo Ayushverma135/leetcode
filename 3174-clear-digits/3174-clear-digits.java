00247c285c0e1939a88ec1407ac9d0e28c2d987d
Ayush verma <120269805+Ayushverma135@users.noreply.github.com>
2024-06-09 18:29:31 +0530

    Time: 2 ms (100.00%), Space: 42.4 MB (100.00%) - LeetHub

---
+++ 3174-clear-digits/3174-clear-digits.java
@@ -0,0 +1,19 @@
+class Solution {
+    public String clearDigits(String s) {
+        Stack<Character> stack = new Stack<>();
+        for (char c : s.toCharArray()) {
+            if (Character.isDigit(c)) {
+                if (!stack.isEmpty()) {
+                    stack.pop();
+                }
+            } else {
+                stack.push(c);
+            }
+        }
+        StringBuilder result = new StringBuilder();
+        for (char c : stack) {
+            result.append(c);
+        }
+        return result.toString();
+    }
+}
