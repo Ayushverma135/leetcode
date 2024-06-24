79687aae5a757376f4f5bac3d15cba7a3f56d2ef
Ayush verma <120269805+Ayushverma135@users.noreply.github.com>
2024-06-25 01:23:10 +0530

    Time: 17 ms (9.38%), Space: 55 MB (90%) - LeetHub

--- 1037-minimum-number-of-k-consecutive-bit-flips/1037-minimum-number-of-k-consecutive-bit-flips.java
+++ 1037-minimum-number-of-k-consecutive-bit-flips/1037-minimum-number-of-k-consecutive-bit-flips.java
@@ -3,12 +3,17 @@ class Solution {
         int count=0;
         int flip=0;
         // boolean arr[]=new boolean[nums.length];
+        Deque<Integer> q=new LinkedList<>();
         for(int i=0;i<nums.length;i++){
             // if(i>=k && arr[i-k]==true){
             //     flip--;
             // }
-            if(i>=k && nums[i-k]==7){
-                flip--;
+
+            // if(i>=k && nums[i-k]==7){
+            //     flip--;
+            // }
+            if(i>=k){
+                flip-=q.poll();
             }
             if((flip%2==1 && nums[i]==1)||(flip%2==0 && nums[i]==0)){
                 if(i+k>nums.length){
@@ -16,9 +21,11 @@ class Solution {
                 }
                 count++;
                 flip++;
-                nums[i]=7;
+                // nums[i]=7;
                 // arr[i]=true;
+                q.add(1);
             }
+            else{q.add(0);}
         }
         return count;
     }
