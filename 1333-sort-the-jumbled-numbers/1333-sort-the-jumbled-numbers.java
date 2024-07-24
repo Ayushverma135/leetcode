f6d70c73b76073736f26c234d9f8fabb926a5cfa
Ayush verma <120269805+Ayushverma135@users.noreply.github.com>
2024-07-24 10:38:14 +0530

    Time: 870 ms (7.84%), Space: 56.9 MB (8.82%) - LeetHub

---
+++ 1333-sort-the-jumbled-numbers/1333-sort-the-jumbled-numbers.java
@@ -0,0 +1,62 @@
+class Solution {
+    public int reverse(int num){
+        int sum=0;
+        if(num==0){
+            return 0;
+        }
+        while(num>0){
+            sum=sum*10+num%10;
+            num/=10;
+        }
+        return sum;
+    }
+    public int[] sortJumbled(int[] mapping, int[] nums) {
+        int n=nums.length;
+        int arr[]=new int[n];
+        for(int i=0;i<n;i++){
+            int temp=nums[i];
+            int newTemp=0;
+            int m=1;
+            while(temp>0){
+                int a=temp%10;
+                newTemp=mapping[a]*m+newTemp;
+                m*=10;
+                temp/=10;
+            }
+            if(nums[i]==0){
+                arr[i]=mapping[0];
+            }
+            else{
+                arr[i]=newTemp;
+            }  
+        }
+        HashMap<Integer,List<Integer>> map=new HashMap<>();
+        for(int i=0;i<n;i++){
+            if(map.containsKey(arr[i])){
+                map.get(arr[i]).add(nums[i]);
+            }
+            else{
+                map.put(arr[i],new ArrayList<>());
+                map.get(arr[i]).add(nums[i]);
+            }
+            
+        }
+        Arrays.sort(arr);
+        List<Integer> res=new ArrayList<>();
+        for(int i=0;i<n;i++){
+            if(map.get(arr[i]).size()>1){
+                res.add(map.get(arr[i]).get(0));
+                map.get(arr[i]).remove(0);
+            }
+            else{
+                res.add(map.get(arr[i]).get(0));
+            }
+            System.out.println(arr[i]);
+        }
+        for(int i=0;i<n;i++){
+            nums[i]=res.get(i);
+        }
+        // int[] obj=res.toArray();
+        return nums;
+    }
+}
